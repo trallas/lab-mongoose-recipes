936f134eca94269b5b8f865a110702fd107ec516
Xavi <xaviertralleroguillot@gmail.com>
2023-05-08 15:20:27 +0200

    solved lab?

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://recipe-app';
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  
+    let recipe1 = {
+      "title": "Asian Glazed Chicken",
+      "level": "Amateur Chef",
+      "ingredients": [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce"
+      ],
+      "cuisine": "Asian",
+      "dishType": "main_course",
+      "image": "../lab-mongoose-recipes/images/815964.jpg"
+      "duration": 60,
+      "creator": "Chef LePapu"
+    }
+
+    return Recipe.create(recipe1)
+  })
+  .then((x)=>{
+    console.log("Single recipe added: ", x)
+  })
+  //Iteration 3:
+  .then(()=>{
+    return Recipe.insertMany(data)
+  })
+  .then((x)=>{
+    console.log("Array of recipes added: ", x)
+  })
+  //Iteration 4:
+  .then(()=>{
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+  })
+  .then(()=>{
+    console.log("Recipe updated successfully!")
+  })
+  //Iteration 5:
+  .then(()=>{
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(()=>{
+    console.log("Recipe deleted successfully!")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
